0a638784311fa921fab6f4f577f3cd1e3321ba7e
tilanayaje <3p1f123@gmail.com>
2025-05-15 00:48:35 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+const button = document.getElementById('button');
+button.addEventListener("click", myFunction);
+const count = document.getElementById('val');
+
+function myFunction() {
+    let valText = document.getElementById("val").textContent;
+    let valNum = Number(valText);
+    valNum += 1;
+    document.getElementById("val").textContent = valNum;
+}
+
