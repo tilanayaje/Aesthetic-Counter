6ce572e42221fd40e8f670157011130e33b6c857
tilanayaje <3p1f123@gmail.com>
2025-05-15 20:08:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,17 @@
 const button = document.getElementById('button');
-button.addEventListener("click", myFunction);
 const count = document.getElementById('val');
+
+let valNum = Number(localStorage.getItem("name"));
+if (isNaN(valNum)) {
+    valNum = 0;
+}
+
+button.addEventListener("click", myFunction);
 
 function myFunction() {
     let valText = document.getElementById("val").textContent;
     let valNum = Number(valText);
     valNum += 1;
     document.getElementById("val").textContent = valNum;
-    
     localStorage.setItem("name", valNum);
-    console.log(localStorage.getItem("name"));
 }
